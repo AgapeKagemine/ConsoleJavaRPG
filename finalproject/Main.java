dc0b27aafd5487f7003f64bbc485e94a72487127
AgapeKagemine <duasahabat123@gmail.com>
2020-05-28 18:47:21 +0700

    Update Main.java

--- finalproject/Main.java
+++ finalproject/Main.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 /*
+                     Data that avalaible when started running
         +===============================================================+
         + Food                         |   Price                        +
         +------------------------------|--------------------------------+
